a3f4f9730077a881660c59557ab5767c6ec8b2ba
Sibaprasad Maiti <sibu.it13@gmail.com>
2023-02-17 00:11:45 +0530

    default route handler added

---
+++ src/app.rs
@@ -0,0 +1,26 @@
+use axum::body::Body;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::{get, IntoMakeService};
+use axum::{Json, Router};
+use serde::Serialize;
+
+pub fn build_app() -> IntoMakeService<Router> {
+    tracing::debug!("Initializing the app");
+    let app: Router<(), Body> = Router::new().route("/", get(default_route_handler));
+    app.into_make_service()
+}
+
+#[derive(Serialize)]
+struct DefaultResponse {
+    success: bool,
+    message: String,
+}
+
+async fn default_route_handler() -> impl IntoResponse {
+    let response = DefaultResponse {
+        success: true,
+        message: "Server is running".to_string(),
+    };
+    (StatusCode::OK, Json(response))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,32 @@
-use axum::http::StatusCode;
-use axum::response::IntoResponse;
-use axum::routing::get;
-use axum::Json;
-use axum::Router;
 use dotenvy::dotenv;
-use serde::Serialize;
-use std::env;
 use std::net::SocketAddr;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+mod app;
 
 #[tokio::main]
 async fn main() {
-    println!("Initializing the app...");
+    // create default env filter
+    let env_filter = tracing_subscriber::EnvFilter::try_from_default_env()
+        .unwrap_or("trailsbuddy_backend_rust=debug".into());
+
+    // initialize tracing subscriber for logging
+    tracing_subscriber::registry()
+        .with(env_filter)
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // import .env file
     dotenv().ok();
-    // initialize tracing
-    tracing_subscriber::fmt::init();
-    // build the app with route
-    let app = Router::new().route("/", get(root));
-    // .route("/users", post(create_user));
-    let port = env::var("PORT").unwrap();
-    let port = port.parse::<u16>().unwrap();
-    let addr = SocketAddr::from(([127, 0, 0, 1], port));
-    tracing::info!("listening on {}", addr);
 
-    // start the server
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
-
-#[derive(Debug, Serialize)]
-struct RootResponse {
-    success: bool,
-    message: String,
-}
-
-// basic handler that responds with a static string
-async fn root() -> impl IntoResponse {
-    let response = RootResponse {
-        success: true,
-        message: "Server is running".to_owned(),
-    };
-
-    (StatusCode::OK, Json(response))
+    // read the port number from env variable
+    let port = std::env::var("PORT").unwrap_or_default();
+    let port = port.parse::<u16>().unwrap_or(3000);
+    // build the socket address
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    // create the app instance
+    let app = app::build_app();
+    tracing::debug!("Starting the app in: {addr}");
+    // start serving the app in the socket address
+    axum::Server::bind(&addr).serve(app).await.unwrap();
 }
