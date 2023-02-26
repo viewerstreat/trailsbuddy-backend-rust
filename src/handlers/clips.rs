03c1fe0192121426a3b918ccd5bbae6034ddfeb1
Sibaprasad Maiti <sibu.it13@gmail.com>
2023-02-26 23:07:38 +0530

    Unit test added for get_clips_handler

--- src/handlers/clips.rs
+++ src/handlers/clips.rs
@@ -17,7 +17,7 @@ use crate::{constants::*, utils::error_handler::AppError};
 #[double]
 use crate::database::AppDatabase;
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 pub struct Clips {
     #[serde(deserialize_with = "hex_string_as_object_id::deserialize")]
     _id: String,
@@ -47,7 +47,7 @@ pub struct Response {
     data: Vec<Clips>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
-    message: Option<&'static str>,
+    message: Option<String>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -95,7 +95,11 @@ fn create_find_options(params: &Query<Params>) -> FindOptions {
     // calculate skip and limit value from query params pageIndex and pageSize
     let page_index = params.page_index.unwrap_or(0);
     let page_size = params.page_size.unwrap_or(DEFAULT_QUERY_LIMIT);
-    let skip = page_index * page_size;
+    let mut skip = page_index * page_size;
+    // when searched by id page_index will be reset to zero
+    if params.id.is_some() {
+        skip = 0;
+    }
     // create sort_by doc
     let sort_by = doc! {"_id": -1};
     // create FindOptions
@@ -120,7 +124,7 @@ mod tests {
 
     use super::*;
 
-    fn get_clips() -> Vec<Clips> {
+    fn get_test_clips() -> Vec<Clips> {
         let mut clips = vec![];
         let clip = Clips {
             _id: ObjectId::new().to_hex(),
@@ -148,9 +152,43 @@ mod tests {
         clips
     }
 
+    fn check_sort() -> Box<dyn Fn(&Document) -> Option<()>> {
+        let closure = |sort: &Document| {
+            if sort.iter().count() > 1 {
+                return None;
+            }
+            sort.get_i32("_id")
+                .ok()
+                .and_then(|val| if val == -1 { Some(()) } else { None })
+        };
+        Box::new(closure)
+    }
+
+    fn check_limit(limit: u64) -> Box<dyn Fn(i64) -> Option<()>> {
+        let closure = move |val: i64| {
+            if val == limit as i64 {
+                Some(())
+            } else {
+                None
+            }
+        };
+        Box::new(closure)
+    }
+
+    fn check_skip(skip: u64) -> Box<dyn Fn(u64) -> Option<()>> {
+        let closure = move |val: u64| {
+            if val == skip {
+                Some(())
+            } else {
+                None
+            }
+        };
+        Box::new(closure)
+    }
+
     #[tokio::test]
     async fn test_get_clips_handler() {
-        let clips = get_clips();
+        let clips = get_test_clips();
         let filter = Some(doc! {"isActive": true});
         let check_options = function(|options: &Option<FindOptions>| {
             options
@@ -159,37 +197,23 @@ mod tests {
                     option
                         .sort
                         .as_ref()
-                        .and_then(|sort| {
-                            if sort.iter().count() > 1 {
-                                return None;
-                            }
-                            sort.get_i32("_id").ok().and_then(|val| {
-                                if val == -1 {
-                                    Some(())
-                                } else {
-                                    None
-                                }
-                            })
-                        })
+                        .and_then(check_sort())
                         .and(option.limit)
-                        .and_then(|limit| {
-                            if limit == DEFAULT_QUERY_LIMIT as i64 {
-                                Some(())
-                            } else {
-                                None
-                            }
-                        })
+                        .and_then(check_limit(DEFAULT_QUERY_LIMIT))
                         .and(option.skip)
-                        .and_then(|skip| if skip == 0 { Some(()) } else { None })
+                        .and_then(check_skip(0))
                 })
                 .is_some()
         });
         let mut mock_db = AppDatabase::default();
-        mock_db
-            .expect_find::<Clips>()
-            .with(eq(DB_NAME), eq(COLL_CLIPS), eq(filter), check_options)
-            .times(1)
-            .returning(move |_, _, _, _| Ok(clips.clone()));
+        {
+            let clips = clips.clone();
+            mock_db
+                .expect_find::<Clips>()
+                .with(eq(DB_NAME), eq(COLL_CLIPS), eq(filter), check_options)
+                .times(1)
+                .returning(move |_, _, _, _| Ok(clips.clone()));
+        }
         let db = Arc::new(mock_db);
         let app = Router::new()
             .route("/", get(get_clips_handler))
@@ -197,5 +221,110 @@ mod tests {
         let req = Request::builder().uri("/").body(Body::empty()).unwrap();
         let res = app.oneshot(req).await.unwrap();
         assert_eq!(res.status(), StatusCode::OK);
+        let bd = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let response: Response = serde_json::from_slice(&bd).unwrap();
+        assert_eq!(response.success, true);
+        assert_eq!(response.data.len(), clips.len());
+        assert_eq!(response.data[0], clips[0]);
+        assert_eq!(response.data[1], clips[1]);
+    }
+
+    #[tokio::test]
+    async fn test_get_clips_handler_pagination() {
+        let clips = get_test_clips();
+        let page_index = 1u64;
+        let page_size = 10u64;
+        let filter = Some(doc! {"isActive": true});
+        let check_options = function(move |options: &Option<FindOptions>| {
+            options
+                .as_ref()
+                .and_then(|option| {
+                    option
+                        .sort
+                        .as_ref()
+                        .and_then(check_sort())
+                        .and(option.limit)
+                        .and_then(check_limit(page_size))
+                        .and(option.skip)
+                        .and_then(check_skip(page_size * page_index))
+                })
+                .is_some()
+        });
+        let mut mock_db = AppDatabase::default();
+        {
+            let clips = clips.clone();
+            mock_db
+                .expect_find::<Clips>()
+                .with(eq(DB_NAME), eq(COLL_CLIPS), eq(filter), check_options)
+                .times(1)
+                .returning(move |_, _, _, _| Ok(clips.clone()));
+        }
+        let db = Arc::new(mock_db);
+        let app = Router::new()
+            .route("/", get(get_clips_handler))
+            .with_state(db);
+        let uri = format!("/?pageSize={}&pageIndex={}", page_size, page_index);
+        let req = Request::builder().uri(uri).body(Body::empty()).unwrap();
+        let res = app.oneshot(req).await.unwrap();
+        assert_eq!(res.status(), StatusCode::OK);
+        let bd = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let response: Response = serde_json::from_slice(&bd).unwrap();
+        assert_eq!(response.success, true);
+        assert_eq!(response.data.len(), clips.len());
+        assert_eq!(response.data[0], clips[0]);
+        assert_eq!(response.data[1], clips[1]);
+    }
+
+    #[tokio::test]
+    async fn test_get_clips_by_id() {
+        let page_index = 1u64;
+        let page_size = 10u64;
+        let clips = get_test_clips();
+        let clip = clips[0].clone();
+        let clip_id = clip._id.clone();
+        let oid = ObjectId::parse_str(clip_id.clone()).unwrap();
+        let filter = Some(doc! {"isActive": true, "_id": oid});
+        let check_options = function(move |options: &Option<FindOptions>| {
+            options
+                .as_ref()
+                .and_then(|option| {
+                    option
+                        .sort
+                        .as_ref()
+                        .and_then(check_sort())
+                        .and(option.limit)
+                        .and_then(check_limit(page_size))
+                        .and(option.skip)
+                        .and_then(check_skip(0))
+                })
+                .is_some()
+        });
+        let mut mock_db = AppDatabase::default();
+        {
+            let clip = clip.clone();
+            mock_db
+                .expect_find::<Clips>()
+                .with(eq(DB_NAME), eq(COLL_CLIPS), eq(filter), check_options)
+                .times(1)
+                .returning(move |_, _, _, _| Ok(vec![clip.clone()]));
+        }
+        let db = Arc::new(mock_db);
+        let app = Router::new()
+            .route("/", get(get_clips_handler))
+            .with_state(db);
+        let uri = format!(
+            "/?_id={}&pageSize={}&pageIndex={}",
+            clip_id.as_str(),
+            page_size,
+            page_index
+        );
+        let req = Request::builder().uri(uri).body(Body::empty()).unwrap();
+        let res = app.oneshot(req).await.unwrap();
+        assert_eq!(res.status(), StatusCode::OK);
+        let bd = hyper::body::to_bytes(res.into_body()).await.unwrap();
+        let response: Response = serde_json::from_slice(&bd).unwrap();
+        assert_eq!(response.success, true);
+        assert_eq!(response.data.len(), 1);
+        assert_eq!(response.data[0], clip);
     }
 }
